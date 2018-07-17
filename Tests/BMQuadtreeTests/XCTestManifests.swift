40fab7268e673cd4038779bd76e91ab344d788ac
Adam Eri <adam.eri@blackmirror.media>
2018-07-17 22:31:29 +0200

    Added tests and performance measurement.

--- Tests/BMQuadtreeTests/XCTestManifests.swift
+++ Tests/BMQuadtreeTests/XCTestManifests.swift
@@ -1,9 +1,9 @@
 import XCTest
 
 #if !os(macOS)
-public func allTests() -> [XCTestCaseEntry] {
-    return [
-        testCase(BMQuadtreeTests.allTests),
-    ]
-}
+//public func allTests() -> [XCTestCaseEntry] {
+//    return [
+//        testCase(BMQuadtreeTests.allTests),
+//    ]
+//}
 #endif
