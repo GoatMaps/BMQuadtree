46bd225bf8c7c50e969ceea572d0ed9c507be89a
Adam Eri <adam.eri@blackmirror.media>
2018-12-05 00:04:04 +0100

    Cleanup

--- Example/Pods/Target Support Files/BMQuadtree/BMQuadtree-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_BMQuadtree : NSObject
-@end
-@implementation PodsDummy_BMQuadtree
-@end
--- Example/Pods/Target Support Files/Pods-BMQuadtree_Tests/Pods-BMQuadtree_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BMQuadtree_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_BMQuadtree_Tests
-@end
