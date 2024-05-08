efacbec03be05848a6d16f0ddbea033606f22ec6
Jesse Crocker <jesse@datamongers.net>
2024-05-08 12:04:53 -0600

    add package.swift file

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.3.
+import PackageDescription
+
+let package = Package(
+    name: "BMQuadTree",
+    platforms: [
+        .macOS(.v10_14), .iOS(.v13), .tvOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "BMQuadTree",
+            targets: ["BMQuadTree"])
+    ],
+    targets: [
+        .target(
+            name: "BMQuadTree",
+            dependencies: [], 
+            path: "BMQuadTree/Classes",
+        )
+    ]
+)
